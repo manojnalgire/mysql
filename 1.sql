ce62f00900099c685068627bdb94f9b92107cef9
Krishna Nalgire <manojnalgire@gmail.com>
2021-08-18 10:19:12 -0700

    1 creating database 2 using database 3 checking table exists 4 creating table 5 inserting records

---
+++ 1.sql
@@ -0,0 +1,22 @@
+-- create database
+CREATE DATABASE Tutorials;
+
+-- create database
+USE Tutorials;
+
+-- Checking if table already exist or not then creating table
+DROP TABLE IF EXISTS Instructor;
+
+-- creating table
+CREATE TABLE Instructor
+(
+id int(11) not NULL AUTO_INCREMENT,
+Name varchar(128),
+Primary key(id)
+);
+
+-- Insert records into the data table
+
+insert into Instructor(Name) values ('Krishna');
+insert into Instructor(Name) values ('Mohan');
+insert into Instructor(Name) values ('Varsha');
